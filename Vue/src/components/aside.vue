8f86644d93fa3677937d378d7e4005032c177dcf
zhaozhen-com <956632755@qq.com>
2019-04-02 15:46:53 +0800

    push lightdoc

---
+++ Vue/src/components/aside.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="container">
+		<v-search></v-search>
+		<div class="tree">
+			<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+		</div>
+	</div>
+</template>
+
+<script>
+import vSearch from './search.vue'
+export default {
+	 data() {
+      return {
+        data: [],
+        defaultProps: {
+          children: 'Folder',
+          label: 'Name'
+        }
+      };
+    },
+		components:{
+			vSearch
+		},
+		mounted(){
+			this.axios.patch('/api').then((res)=>{
+				this.data=res.data.Folder
+				console.log(res)
+			})
+		},
+    methods: {
+      handleNodeClick(data) {
+        
+      }
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+	.container
+		display:flex
+		flex-direction:column
+		width:30%
+		height:100%
+		//background-color:#ccc
+		max-width:224px
+		border-right:1px solid #ccc
+</style>
---
+++ Vue/src/views/index.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="container">
+	    <v-header></v-header>
+		<div class="content">
+			<v-aside class="hidden-xs-only"></v-aside>
+			<v-markdown></v-markdown>
+		</div>
+		<sm-show class="smShow hidden-sm-and-up"></sm-show>
+	</div>
+</template>
+<script>
+import vHeader from '../components/header.vue'
+import vAside from '../components/aside.vue'
+import vMarkdown from '../components/markdown.vue'
+import smShow from '../components/smShow.vue'
+import 'element-ui/lib/theme-chalk/display.css';
+export default {
+  components: {
+	vHeader,
+	vAside,
+	vMarkdown,
+	smShow
+  },
+  methods: {}
+}
+</script>
+<style lang="stylus" scoped>
+	.container
+		position:relative
+		display:flex
+		flex-direction:column
+		height:100%
+		.content
+			display:flex
+			flex-direction:row
+			height:100%
+		@media screen and (max-width:768px)
+			.smShow
+				background:#CCC
+				width:100%
+				height:auto
+				position:absolute
+				top:7%
+				left:0
+				display:block
+</style>
