57fbc1bc471e84ad0a4ffc489ca35c6a896b6a1c
Ke Zhang <4evertomchristmas@gmail.com>
2019-07-02 17:13:31 +0800

    input

--- vue/src/components/search.vue
+++ vue/src/components/search.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="search">
-    <input disabled class="inpt" v-model="searchContent" placeholder="搜索暂不可用！">
-    <i class="el-icon-search iconSearch"></i>
+    <el-input
+      type="text"
+      placeholder="请输入内容"
+      clearable
+      prefix-icon="el-icon-search"
+      v-model="searchContent"
+    ></el-input>
   </div>
 </template>
 <script>
@@ -21,21 +26,8 @@ export default {
 <style lang="stylus" scoped>
 .search {
   position: relative;
-
-  .inpt {
-    width: 100%;
-    text-indent: 40px;
-    display: block;
-    height: 30px;
-    background: rgba(0, 0, 0, 0.1);
-    // border-bottom:1px solid #ccc
-    // border-top:1px solid #ccc
-  }
-
-  .iconSearch {
-    position: absolute;
-    left: 5%;
-    top: 25%;
-  }
+  width: 90%;
+  left: 5%;
+  right: 5%;
 }
 </style>
