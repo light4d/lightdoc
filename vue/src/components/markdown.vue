1c0c14328a2670e934ec19889308e0d9a016d466
Ke Zhang <4evertomchristmas@gmail.com>
2019-07-08 16:30:59 +0800

    loading and warning page

--- vue/src/components/markdown.vue
+++ vue/src/components/markdown.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="m-container">
-    <div v-html="this.$store.state.readmeContent"></div>
+    <el-table
+      class="load"
+      v-loading="isLoading"
+      element-loading-text="拼命加载中"
+      element-loading-spinner="el-icon-loading"
+      element-loading-background="#DCDFE6"
+      style="width: 100%"
+    ></el-table>
+    <div id="content" v-html="this.$store.state.readmeContent"></div>
+    <el-alert class="alertMsg" center title="请求错误！请刷新重试" type="error" v-show="apiFailure"></el-alert>
   </div>
 </template>
 
@@ -12,12 +21,14 @@ export default {
   name: "vMarkdown",
   data() {
     return {
-      defaultData: "preview",
-      isLoading: false
+      isLoading: false,
+      timer: "",
+      apiFailure: false
     };
   },
   watch: {
     $route(to, from) {
+      this.isLoading = true;
       this.axios({
         method: "GET",
         url: to.path,
@@ -25,13 +36,16 @@ export default {
       })
         .then(res => {
           this.$store.state.readmeContent = marked(res.data);
+          this.isLoading = false;
         })
         .catch(err => {
           console.log(111);
+          this.apiFailure = true;
         });
     }
   },
   created() {
+    this.isLoading = true;
     this.axios({
       method: "GET",
       url: "/" + this.$route.params.pageId,
@@ -39,29 +53,34 @@ export default {
     })
       .then(res => {
         this.$store.state.readmeContent = marked(res.data);
+        this.isLoading = false;
       })
       .catch(err => {
         console.log(111);
       });
-    console.log(this.$store.state.readmeContent);
-    marked.setOptions({
-      renderer: new marked.Renderer(),
-      highlight: function(readmeContent) {
-        return hljs.highlightAuto(readmeContent).value;
-      },
-      pedantic: false,
-      gfm: true,
-      tables: true,
-      breaks: false,
-      sanitize: false,
-      smartLists: true,
-      smartypants: false,
-      xhtml: false
-    });
+    this.timer = setTimeout(this.apiFailed, 8000);
+    // console.log(this.$store.state.readmeContent);
+    // marked.setOptions({
+    //   renderer: new marked.Renderer(),
+    //   highlight: function(readmeContent) {
+    //     return hljs.highlightAuto(readmeContent).value;
+    //   },
+    //   pedantic: false,
+    //   gfm: true,
+    //   tables: true,
+    //   breaks: false,
+    //   sanitize: false,
+    //   smartLists: true,
+    //   smartypants: false,
+    //   xhtml: false
+    // });
   },
   methods: {
     changeData(value, render) {
       console.log(render);
+    },
+    apiFailed() {
+      this.isLoading = false;
     }
   }
 };
@@ -70,7 +89,20 @@ export default {
 <style lang="stylus" scoped>
 // @import '../../static/monokai-sublime.css';
 .m-container {
-  padding: 0 30px 0 30px;
+  display: flex;
+  padding: 0 10px;
   width: auto;
+}
+
+.alertMsg {
+  position: absolute;
+  width: auto;
+  top: 10%;
+  left: 50%;
+  transform: translateX(40%);
+}
+
+#content {
+  width: 100%;
 }
 </style>
